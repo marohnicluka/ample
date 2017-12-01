a9ccb9bc63e51ff72f74b944f161a2fc5c53ab64
marohnicluka <marohnicluka@gmail.com>
2017-12-01 03:14:46 +0100

    Updated all files

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,28 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QActionGroup *alignGroup = new QActionGroup(this);
+    alignGroup->addAction(ui->actionAlignLeft);
+    alignGroup->addAction(ui->actionAlignCenter);
+    alignGroup->addAction(ui->actionAlignRight);
+    alignGroup->addAction(ui->actionAlignJustify);
+    alignGroup->setExclusive(true);
+    connect(alignGroup, SIGNAL(triggered(QAction*)), this, SLOT(textAlign(QAction*)));
+}
+
+void MainWindow::textAlign(QAction *a) {
+    if (a == ui->actionAlignLeft)
+        qDebug("Left");
+    //textEdit->setAlignment(Qt::AlignLeft | Qt::AlignAbsolute);
+    else if (a == ui->actionAlignCenter)
+        qDebug("Center");
+    //textEdit->setAlignment(Qt::AlignHCenter);
+    else if (a == ui->actionAlignRight)
+        qDebug("Right");
+    //textEdit->setAlignment(Qt::AlignRight | Qt::AlignAbsolute);
+    else if (a == ui->actionAlignJustify)
+        qDebug("Justified");
+    //textEdit->setAlignment(Qt::AlignJustify);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QComboBox>
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,10 @@ public:
 
 private:
     Ui::MainWindow *ui;
+
+private slots:
+    void textAlign(QAction *a);
+
 };
 
 #endif // MAINWINDOW_H
