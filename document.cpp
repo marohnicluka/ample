04da327d0896bc597f43e892d57cf060fcf92733
marohnicluka <marohnicluka@gmail.com>
2017-12-11 23:38:37 +0100

    Updated license headers

--- document.cpp
+++ document.cpp
@@ -1,18 +1,18 @@
 /*
- * This file is part of Ample Editor.
+ * This file is part of Ample.
  *
- * Ample Editor is free software: you can redistribute it and/or modify
+ * Ample is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Ample Editor is distributed in the hope that it will be useful,
+ * Ample is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Ample Editor.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Ample.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <QTextTableCell>
--- mainwindow.h
+++ mainwindow.h
@@ -1,18 +1,18 @@
 /*
- * This file is part of Giac Qt.
+ * This file is part of Ample.
  *
- * Giac Qt is free software: you can redistribute it and/or modify
+ * Ample is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Giac Qt is distributed in the hope that it will be useful,
+ * Ample is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Giac Qt.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Ample.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef MAINWINDOW_H
