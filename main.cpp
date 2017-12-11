2b960cbae76b738920a5a70f380f579c459601b1
marohnicluka <marohnicluka@gmail.com>
2017-12-11 21:07:04 +0100

    Finished changing project name

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QCoreApplication::setApplicationName("GiacQt");
+    QCoreApplication::setApplicationName("Ample");
     MainWindow w;
     w.show();
     return a.exec();
